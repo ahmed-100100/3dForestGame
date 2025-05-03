10b9ac13951b0723cbb753932520177ca3ecc5b7
Ahmed Hassan <ahmedaemara078@gmail.com>
2025-05-03 04:28:34 +0300

    Score & Sound Effects

--- Assets/scripts/Cactus.cs
+++ Assets/scripts/Cactus.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class Cactus : MonoBehaviour
 {
+    public AudioClip hurtSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            ScoreManager.instance.AddScore(-1); 
-            Destroy(gameObject); 
+            audioSource.PlayOneShot(hurtSound);
+            ScoreManager.instance.AddScore(-1);
+            Destroy(gameObject, hurtSound.length/2);
         }
     }
 }
--- Assets/scripts/Fruits.cs
+++ Assets/scripts/Fruits.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    public AudioClip collectSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            audioSource.PlayOneShot(collectSound);
             ScoreManager.instance.AddScore(1);
-            Destroy(gameObject);
+            Destroy(gameObject, collectSound.length/2);
         }
     }
 }
-
